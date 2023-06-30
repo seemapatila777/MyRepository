8989af714c72b71bc4c3be4dd53db5b98a5a8162
seemapatila777 <patilseemam3@gmail.com>
2023-06-30 13:18:55 +0530

    second commit

---
+++ Ifelse.java
@@ -0,0 +1,21 @@
+
+public class Ifelse {
+	public static void main(String[] args) {
+		
+		int a=56;
+		
+		if(a<=50) {
+			System.out.println("Grade: C");
+		}
+		else if(a>=50 && a<=60) {
+			System.out.println("Grade : B");
+		}
+		else if(a>=61 && a<=70) {
+			System.out.println("Grade : A");
+		}
+		else {
+			System.out.println("Fail");
+		}
+	}
+
+}
